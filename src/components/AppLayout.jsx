282cf2822796773b42366504f92014ad9173ab7d
Mahyar <mahyar.erfanian1998@gmail.com>
2024-02-16 01:56:36 -0500

    Set up the layout structure with a header and a footer as well as basic styling for this component

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,3 +1,34 @@
-const AppLayout = () => {};
+import '../styles/AppLayout.css'
+import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+
+const AppLayout = ({ content }) => {
+  return (
+    <div className="layout">
+      <header>
+        <h1>Fake Shopping Website</h1>
+        <ul>
+          <Link to="home">Home</Link>
+          <Link to="shop">Shop</Link>
+          <Link to="cart">Cart</Link>
+        </ul>
+      </header>
+      <main> {content} </main>
+      <footer>
+        <p>
+          Check out my GitHub:{" "}
+          <a href="https://github.com/Mahyar-98">
+            <FaGithub />
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+    content: PropTypes.node.isRequired
+}
 
 export default AppLayout;
